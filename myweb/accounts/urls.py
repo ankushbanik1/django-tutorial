ed925999c27b2c42270f3708aaa1f1d47515fc2c
ankush banik <ankushbanik1234@gmail.com>
2018-11-23 00:09:39 +0530

    creat login page

--- myweb/accounts/urls.py
+++ myweb/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url 
-
 from. import views
+from django.contrib.auth import views as auth_views
 urlpatterns=[
     url(r'^$', views.home),
+       url( r'^login/$',auth_views.LoginView.as_view(template_name="accounts/home1.html"), name="login1"),
 
 
 ]
