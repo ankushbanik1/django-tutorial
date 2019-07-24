21877bebd6cec7e28eac50cbf14d9d18517cae65
ankush banik <ankushbanik123@gmail.com>
2019-07-24 09:31:15 +0530

    make it better

--- website3/website3/settings.py
+++ website3/website3/settings.py
@@ -10,10 +10,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '78xbhcn4q(z)vnq=two!k*do8$s#dgy51$ackxj70ol3kkolrv'
+#SECRET_KEY = '78xbhcn4q(z)vnq=two!k*do8$s#dgy51$ackxj70ol3kkolrv'
 
+SECRET_KEY =os.environ.get("BLOG_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
